a35fd1f522226b92da33555c7c1a4cefca3afc98
PavloSukhylin <supana1976@gmail.com>
2021-03-25 22:58:35 +0300

    Correct view HW3

---
+++ src/CheckParityNumber.java
@@ -0,0 +1,8 @@
+public class CheckParityNumber {
+    public static void main(String[] args) {
+        int number = 27;
+        int remainder = number % 2;
+        String conclusion = (remainder == 0) ? ("Число " + number + " четное") : ("Число " + number + " нечетное");
+        System.out.println(conclusion);
+    }
+}
